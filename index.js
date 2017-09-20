4237376cad580728b516bd7d2cd0b719ec0df72f
github username <github email address>
2017-09-20 09:11:17 -0400

    pushing master

---
+++ index.js
@@ -0,0 +1,8 @@
+$('#save_lincoln').click(()=>saveLincoln(parseInt($('#interval').val())))
+
+function saveLincoln(interval) {
+  if(isNaN(interval)) {
+    interval = 10000
+  }
+  $('#foreground').fadeOut(interval);
+}
